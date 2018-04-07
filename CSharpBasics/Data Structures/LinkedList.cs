ffa2e3abeaeef22aebb02c6944e4e52d4efe0b2d
Matt Gross <mattkgross@gmail.com>
2018-04-07 08:49:34 -0600

    Insert complete.

--- CSharpBasics/Data Structures/LinkedList.cs
+++ CSharpBasics/Data Structures/LinkedList.cs
@@ -61,11 +61,31 @@ namespace CSharpBasics
             this.IsDoublyLinked = isDoublyLinked;
             this.Data = data;
             this.Next = next;
-            this.prev = this.IsDoublyLinked ? prev : null;
+            this.Prev = prev;
+        }
+
+        /// <summary>
+        /// Makes a hard copy of the node passed in.
+        /// </summary>
+        /// <param name="copy">Copy.</param>
+        public Node(Node<T> copy)
+        {
+            if (copy == null)
+            {
+                throw new ArgumentException("A non-null node must be passed in for copying.");
+            }
+
+            this.IsDoublyLinked = copy.IsDoublyLinked;
+            this.Data = copy.Data;
+            this.Next = copy.Next;
+            this.Prev = copy.Prev;
         }
         #endregion Constructors
     }
 
+    /// <summary>
+    /// Linked list. Type T must have a public parameterless constructor.
+    /// </summary>
     class LinkedList<T> where T : new()
     {
         #region Variables
@@ -97,15 +117,24 @@ namespace CSharpBasics
         #endregion Properties
 
         #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:CSharpBasics.LinkedList`1"/> class.
+        /// </summary>
+        /// <param name="head">The head node. A hard copy is made and inserted into the list.</param>
         public LinkedList(Node<T> head)
         {
-            this.Head = head ?? throw new ArgumentException("A linked list requires a head node.");
             this.IsDoublyLinked = head.IsDoublyLinked;
+            this.Head = new Node<T>(head);
 
-            // Set the tail. O(n).
-            this.Tail = head;
+            // Sanity check. We don't want any previous links for the head.
+            this.Head.Prev = null;
+
+            // Make hard copies of the rest of the nodes and set the tail. O(n).
+            this.Tail = this.Head;
             while (this.Tail.Next != null)
             {
+                this.Tail.Next = new Node<T>(this.Tail.Next);
+                this.Tail.Next.Prev = this.Tail;
                 this.Tail = this.Tail.Next;
                 this.Count++;
             }
@@ -114,57 +143,113 @@ namespace CSharpBasics
 
         #region Methods
         /// <summary>
-        /// Insert the specified node into the list.
+        /// Insert a hard copy of the specified node into the list.
         /// </summary>
         /// <returns></returns>
         /// <param name="node">Node to insert.</param>
         /// <param name="index">Position to insert the node at. Defaults to the end of the list.</param>
-        /*public void Insert(Node<T> node, int index = -1)
+        public void Insert(Node<T> node, int index = -1)
         {
             if (node == null)
             {
                 return;
             }
 
-            if (!NodeIsCompatible(node))
+            Node<T> newNode = new Node<T>(node);
+
+            if (!NodeIsCompatible(newNode))
             {
-                throw new ArgumentException($"Cannot insert node that is {(node.IsDoublyLinked ? string.Empty : "not ")}doubly " +
+                throw new ArgumentException($"Cannot insert node that is {(newNode.IsDoublyLinked ? string.Empty : "not ")}doubly " +
                                             $"linked into a list that is{(this.IsDoublyLinked ? "." : " not.")}");
             }
 
-            // Insert at the end of the list if no index specified.
-            if (index < 0)
+            // Insert at the end of the list if no index specified or it's bigger than the current size.
+            if (index < 0 || this.Count <= index)
             {
                 // If doubly linked, link the previous to the end of our current list.
                 if (this.IsDoublyLinked)
                 {
-                    node.Prev = this.Tail;
+                    newNode.Prev = this.Tail;
                 }
 
-                this.Tail.Next = node;
+                this.Tail.Next = newNode;
+                this.Tail = this.Tail.Next;
+                this.Count++;
 
-                // Set the tail back to the end of the list.
+                // Set the tail back to the end of the list while making hard copies as we go. O(n).
                 while (this.Tail.Next != null)
                 {
+                    this.Tail.Next = new Node<T>(this.Tail.Next);
+                    this.Tail.Next.Prev = this.Tail;
                     this.Tail = this.Tail.Next;
+                    this.Count++;
                 }
             }
             else
             {
                 // If it's doubly linked and the insert point is closer to the tail, then start from the tail.
-                bool startFromHead = !this.IsDoublyLinked || (this.Count / index) > 2;
+                bool startFromHead = !this.IsDoublyLinked || (this.Count * 1.0 / index) > 2.0;
+                uint cur;
+                Node<T> temp, preTemp = null;
 
+                // Could be singly or doubly linked, and we're starting from the head.
                 if (startFromHead)
                 {
+                    cur = 0;
+                    temp = this.Head;
 
+                    while (cur < index)
+                    {
+                        preTemp = temp;
+                        temp = temp.Next;
+                        cur++;
+                    }
                 }
                 // It's doubly linked and should be iterated starting at the tail.
                 else
                 {
-                    uint cur = this.Count - 1;
+                    cur = this.Count - 1;
+                    temp = this.Tail;
+
+                    while (cur > index)
+                    {
+                        temp = temp.Prev;
+                        cur--;
+                    }
+
+                    preTemp = temp.Prev;
+                }
+
+                // Connect to the leading nodes.
+                newNode.Prev = temp.Prev;
+                if (preTemp != null)
+                {
+                    // If we're not inserting at the front, then connect the beginning of the list.
+                    preTemp.Next = newNode;
+                }
+
+                // Make copies of the rest of the chain inserted.
+                Node<T> temp2 = newNode;
+                this.Count++;
+                while (temp2.Next != null)
+                {
+                    temp2.Next = new Node<T>(temp2.Next);
+                    temp2.Next.Prev = temp2;
+                    temp2 = temp2.Next;
+                    this.Count++;
+                }
+
+                // Connect the inserted segment to the trailing nodes.
+                temp.Prev = temp2;
+                temp2.Next = temp;
+
+                // If we inserted at the head, then set it to the new starting node.
+                if (index == 0)
+                {
+                    this.Head = newNode;
                 }
             }
-        }*/
+        }
 
         /// <summary>
         /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:CSharpBasics.LinkedList`1"/>.
