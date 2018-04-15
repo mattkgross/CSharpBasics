802197a886280e4849e8805b509b05966804d93d
Matt Gross <mattkgross@gmail.com>
2018-04-15 07:33:05 -0600

    Graph Node Setup

--- CSharpBasics/Data Structures/Graph.cs
+++ CSharpBasics/Data Structures/Graph.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CSharpBasics.DataStructures
 {
     /* A graph can be represented in 3 ways: OOP, adjacency matrix, or an adjacency list.
@@ -23,10 +25,43 @@ namespace CSharpBasics.DataStructures
      *       Time: O(1) to O(V)
      */
 
-    public class GraphNode
+    public class GraphNode<T>
     {
-        public GraphNode()
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        /// <value>The data.</value>
+        public T Data { get; set; }
+
+        // C# Equivalent of a HashMap (unique keys).
+        /// <summary>
+        /// The neighboring nodes. The key is the Node, the value is the
+        /// weight of the edge from the current node to the neighbor.
+        /// </summary>
+        public Dictionary<GraphNode<T>, int> Neighbors { get; private set; }
+
+        public GraphNode(T data, Dictionary<GraphNode<T>, int> neighbors = null)
         {
+            this.Data = data;
+            this.Neighbors = neighbors ?? new Dictionary<GraphNode<T>, int>();
+        }
+
+        /// <summary>
+        /// Adds the neighbor with its edge wait from the current node.
+        /// </summary>
+        /// <returns><c>true</c>, if neighbor was added, <c>false</c> already there.</returns>
+        /// <param name="neighbor">The neighbor to add.</param>
+        /// <param name="edgeWeight">Edge weight to the neighbor from the current node.</param>
+        public bool AddNeighbor(GraphNode<T> neighbor, int edgeWeight)
+        {
+            // If the neighbor is already in the list, tell them.
+            if (neighbor == null || this.Neighbors.ContainsKey(neighbor))
+            {
+                return false;
+            }
+
+            this.Neighbors.Add(neighbor, edgeWeight);
+            return true;
         }
     }
 }
